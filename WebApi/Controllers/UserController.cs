c22d2ae43af8951050a5a7f420d396f37bbce680
mahmud <mahmudjonb515@gmail.com>
2024-02-23 07:18:53 +0500

    log middlewhere

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using log4net;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.Model;
 using WebApi.Services;
 
@@ -9,21 +10,36 @@ namespace WebApi.Controllers
     public class UserController:ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly ILog _log;
 
         public UserController(IUserService userService)
         {
             _userService = userService;
+            _log = LogManager.GetLogger(typeof(UserController));
         }
 
         [HttpGet("GetUsers")]
-        public  List<User> GetUser() =>   _userService.GetUsers();
+        public List<User> GetUser()
+        {
+            _log.Info("funcsion GetUsers works");
+           return _userService.GetUsers();
+        }
 
         [HttpPost("Post")]
-        public bool PostUser(User user) => _userService.AddUser(user);
+        public bool PostUser(User user)
+        {
+            _log.Info("funcsion GetUsers works");
+            return _userService.AddUser(user);
+        }
+
 
         [HttpGet("GEtById")]
-        public User GetById(int id ) => _userService.GetById(id);
-        [HttpDelete("delete")]
+        public User GetById(int id)
+        {
+            _log.Info($"funcsion  works");
+            return _userService.GetById(id);
+        }
+        [HttpDelete("Delete")]
         public bool Delete(int id) => _userService.Delete(id);
         [HttpPut("Update")]
         public bool Update(User model) => _userService.Update(model);
--- WebApi/MiddleWhere/Log.cs
+++ WebApi/MiddleWhere/Log.cs
@@ -1,38 +1,72 @@
 ﻿using log4net;
-using log4net.Util;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace WebApi.MiddleWhere
+public class Log
 {
-    public class Log
+    private readonly RequestDelegate _next;
+    private readonly ILog _logger;
+
+    public Log(RequestDelegate next, ILoggerFactory loggerFactory)
     {
-       
-            private readonly RequestDelegate _next;
-        private readonly ILog _log;
+        _next = next;
+        _logger = LogManager.GetLogger(typeof (Log));   
+    }
 
-            public Log(RequestDelegate next)
-            {
-                _next = next;
-                _log = LogManager.GetLogger(typeof(Log));
-            }
+    public async Task Invoke(HttpContext context)
+    {
+        // Log the request
+        var request = await FormatRequest(context.Request);
+        _logger.Info(request);
 
-            public async Task Invoke(HttpContext httpContext)
-            {
-                try
-                {
-                _log.InfoExt(httpContext.Response);
-                    await _next(httpContext);
-                }
-                catch (Exception ex)
-                {
-                    var res = (200+ ex.Message);
+        // Store the current response body stream
+        var originalBodyStream = context.Response.Body;
 
-                    httpContext.Response.Clear();
-                    httpContext.Response.ContentType = "application/json";
-                    httpContext.Response.StatusCode = 400;
-                    await httpContext.Response.WriteAsync(res);
+        // Create a new memory stream to capture the response
+        using (var responseBody = new MemoryStream())
+        {
+            // Replace the response body with our stream
+            context.Response.Body = responseBody;
 
-                }
-            
-            }
+            // Continue processing the request
+            await _next(context);
+
+            // Log the response
+            var response = await FormatResponse(context.Response);
+            _logger.Info(response);
+
+            // Copy the response stream to the original body stream
+            await responseBody.CopyToAsync(originalBodyStream);
+        }
+    }
+
+    private async Task<string> FormatRequest(HttpRequest request)
+    {
+        request.EnableBuffering();
+
+        // Read the request body
+        using (var reader = new StreamReader(request.Body, Encoding.UTF8, true, 1024, true))
+        {
+            var body = await reader.ReadToEndAsync();
+            request.Body.Position = 0;
+            return $"{request.Method} {request.Scheme}://{request.Host}{request.Path} {request.QueryString} {body}";
+        }
+    }
+
+    private async Task<string> FormatResponse(HttpResponse response)
+    {
+        response.Body.Seek(0, SeekOrigin.Begin);
+
+        // Read the response body
+        var body = await new StreamReader(response.Body).ReadToEndAsync();
+
+        // Restore the original response body position
+        response.Body.Seek(0, SeekOrigin.Begin);
+
+        return $"HTTP {response.StatusCode} {body}";
     }
 }
